544392ba1e77011f59a3e77dc1f3f23b5f309815
rolemberg <vinnie.nsts@gmail.com>
2024-05-25 15:04:50 -0300

    unit testing on ReserveRepoTest;

--- src/test/java/com/pan/ormJpa/ReserveRepoTest.java
+++ src/test/java/com/pan/ormJpa/ReserveRepoTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 public class ReserveRepoTest {
 
@@ -13,7 +17,10 @@ public class ReserveRepoTest {
 
     @Test
     void testingReserveRepo() {
-
+        assertEquals(5, reserveRepository.findById(3).get().getId_destiny());
+        assertEquals(1, reserveRepository.findById(2).get().getId_user());
+        assertEquals("confirmed", reserveRepository.findById(4).get().getStatus());
+        assertEquals(LocalDate.of(2024, 6, 10), reserveRepository.findById(1).get().getReserve_date());
     }
 }
 
